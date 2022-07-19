25a905e42b4951b5a0dc3a21178ec0ca31d91f0a
Tomáš Mikula <mail@tomasmikula.cz>
2022-07-19 15:50:10 +0200

    Fix tests

--- tests.py
+++ tests.py
@@ -6,15 +6,15 @@ import pytest
 @pytest.mark.parametrize(
     "input_, output",
     [
-        ("10,20", "x^2: 100.0, y^2: 400.0"),
-        ("-12,20", "x^2: 144.0, y^2: 400.0"),
-        ("1.234,0", "x^2: 1.522756, y^2: 0.0"),
-        ("-12.324,-12313", "x^2: 151.880976, y^2: 151609969.0"),
+        ("10,20;20,10", "[{'x': 100.0, 'y': 400.0}, {'x': 400.0, 'y': 100.0}]"),
+        ("-12,20", "[{'x': 144.0, 'y': 400.0}]"),
+        ("1.234,0;10,20", "[{'x': 1.522756, 'y': 0.0}, {'x': 100.0, 'y': 400.0}]"),
+        ("1.234,0;10,20;1.234,0;10,20", "[{'x': 1.522756, 'y': 0.0}, {'x': 100.0, 'y': 400.0}, {'x': 1.522756, 'y': 0.0}, {'x': 100.0, 'y': 400.0}]"),
     ],
 )
 def test_output(script_runner, input_, output):
     ret = script_runner.run("points_input.py", stdin=io.StringIO(input_))
 
     assert ret.success
-    assert f"{output}\n" in ret.stdout
+    assert output in ret.stdout
     assert ret.stderr == ""
